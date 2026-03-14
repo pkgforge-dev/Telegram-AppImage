083725aba7f4258a9e750ccff826708917c73eee
Link4Electronics <link4electronics@gmail.com>
2026-03-13 23:05:28 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,5 +28,6 @@ if [ "$ARCH" = "x86_64" ]; then
 else
 	pacman -S --noconfirm telegram-desktop
 	VERSION=$(pacman -Q telegram-desktop | awk '{print $2; exit}')
+	echo "$VERSION" > ~/version
 	mv -v /usr/bin/Telegram ./AppDir/bin
 fi
