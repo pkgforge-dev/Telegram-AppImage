ae161bd86202420cb756263e76276ed8f2b45916
Link4Electronics <link4electronics@gmail.com>
2026-03-14 21:16:41 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,28 +15,29 @@ pacman -Syu --noconfirm \
 	gst-plugin-va    \
 	pipewire-audio   \
 	pipewire-jack    \
+	telegram-desktop \
 	webkit2gtk-4.1
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
-echo "Getting telegram binary..."
-echo "---------------------------------------------------------------"
-mkdir -p ./AppDir/bin
-if [ "$ARCH" = "x86_64" ]; then
-	TARBALL_LINK=$(wget --retry-connrefused --tries=30 \
-		https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest -O - \
-		| sed 's/[()",{} ]/\n/g' | grep -o -m 1 'https.*releases.*tsetup.*tar.xz'
-	)
-	wget "$TARBALL_LINK" -O /tmp/telegram.tar.xz
-	tar xvf /tmp/telegram.tar.xz
-	rm -f /tmp/telegram.tar.xz
-	mv -v ./Telegram/Telegram ./AppDir/bin
-	echo "$TARBALL_LINK" | awk -F'/' '{print $(NF-1)}' > ~/version
-else
-	pacman -S --noconfirm telegram-desktop
-	VERSION=$(pacman -Q telegram-desktop | awk '{print $2; exit}')
-	echo "$VERSION" > ~/version
-	mv -v /usr/bin/Telegram ./AppDir/bin
-fi
+#echo "Getting telegram binary..."
+#echo "---------------------------------------------------------------"
+#mkdir -p ./AppDir/bin
+#if [ "$ARCH" = "x86_64" ]; then
+#	TARBALL_LINK=$(wget --retry-connrefused --tries=30 \
+#		https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest -O - \
+#		| sed 's/[()",{} ]/\n/g' | grep -o -m 1 'https.*releases.*tsetup.*tar.xz'
+#	)
+#	wget "$TARBALL_LINK" -O /tmp/telegram.tar.xz
+#	tar xvf /tmp/telegram.tar.xz
+#	rm -f /tmp/telegram.tar.xz
+#	mv -v ./Telegram/Telegram ./AppDir/bin
+#	echo "$TARBALL_LINK" | awk -F'/' '{print $(NF-1)}' > ~/version
+#else
+#	pacman -S --noconfirm telegram-desktop
+#	VERSION=$(pacman -Q telegram-desktop | awk '{print $2; exit}')
+#	echo "$VERSION" > ~/version
+#	mv -v /usr/bin/Telegram ./AppDir/bin
+#fi
